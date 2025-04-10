8d3503dac2549c7f995e87726a40ad0aa083d405
BernardoMoschen <bernardomoschen.dev@gmail.com>
2025-04-10 12:24:44 -0300

    chore: adds react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
     site: "https://example.com",
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), react()],
 
     vite: {
         plugins: [tailwindcss()],
