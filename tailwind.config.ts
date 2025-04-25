d95c317e37a490bf45705a4d5fdc9f7bd03fd911
BernardoMoschen <bernardomoschen.dev@gmail.com>
2025-04-25 13:01:26 -0300

    chore: clear tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,6 @@ module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
     theme: {
         extend: {
-            colors: {
-                grayLight: "rgb(229, 233, 240)",
-                marzipan: "#f8d3a0",
-                tangelo: "#fd5200",
-            },
             backgroundImage: {
                 "bg-texture": "url('/images/bg_texture.jpg')",
             },
