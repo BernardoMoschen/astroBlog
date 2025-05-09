a9c9d6f3b7a8accf23eb25d01cabcaf5ee512945
BernardoMoschen <bernardomoschen.dev@gmail.com>
2025-05-09 10:16:00 -0300

    refactor: who and intro components

--- src/components/react/Marquee.tsx
+++ src/components/react/Marquee.tsx
@@ -1,25 +1,51 @@
 export const Marquee = () => {
     return (
-        <div className='relative  w-full flex overflow-x-hidden'>
-            <div className='py-12 w-full  animate-marquee whitespace-nowrap'>
-                <span className='text-4xl mx-4'>Marquee Item 1</span>
-                <span className='text-4xl mx-4'>Marquee Item 2</span>
-                <span className='text-4xl mx-4'>Marquee Item 3</span>
-                <span className='text-4xl mx-4'>Marquee Item 4</span>
-                <span className='text-4xl mx-4'>Marquee Item 5</span>
-                <span className='text-4xl mx-4'>Marquee Item 5</span>
-                <span className='text-4xl mx-4'>Marquee Item 5</span>
-                <span className='text-4xl mx-4'>Marquee Item 5</span>
-                <span className='text-4xl mx-4'>Marquee Item 5</span>
-                <span className='text-4xl mx-4'>Marquee Item 5</span>
+        <div className='relative flex overflow-x-hidden'>
+            <div className='flex py-12 animate-marquee whitespace-nowrap gap-8' w-full >
+                <i className="devicon-javascript-plain colored md:text-7xl text-4xl"></i>
+                <i className="devicon-typescript-plain colored md:text-7xl text-4xl"></i>
+                <i className="devicon-html5-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-css3-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-react-original colored md:text-7xl text-4xl"></i>
+                <i className="devicon-tailwindcss-original colored md:text-7xl text-4xl"></i>
+                <i className="devicon-materialui-plain colored md:text-7xl text-4xl"></i>
+                <i className="devicon-jest-plain colored md:text-7xl text-4xl"></i>
+                <i className="devicon-nodejs-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-prisma-original md:text-7xl text-4xl"></i>
+                <i className="devicon-adonisjs-original md:text-7xl text-4xl colored"></i>
+                <i className="devicon-nestjs-original colored md:text-7xl text-4xl"></i>
+                <i className="devicon-express-original md:text-7xl text-4xl"></i>
+                <i className="devicon-postgresql-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-mysql-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-mongodb-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-microsoftsqlserver-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-docker-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-linux-plain md:text-7xl text-4xl" ></i>
+                <i className="devicon-git-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-gitlab-plain-wordmark colored md:text-7xl text-4xl"></i>
             </div>
-
-            <div className='absolute w-full  top-0 py-12 animate-marquee2 whitespace-nowrap'>
-                <span className='text-4xl mx-4'>Marquee Item 1</span>
-                <span className='text-4xl mx-4'>Marquee Item 2</span>
-                <span className='text-4xl mx-4'>Marquee Item 3</span>
-                <span className='text-4xl mx-4'>Marquee Item 4</span>
-                <span className='text-4xl mx-4'>Marquee Item 5</span>
+            <div className='flex absolute top-0 py-12 animate-marquee2 whitespace-nowrap gap-12 w-full'>
+                <i className="devicon-javascript-plain colored md:text-7xl text-4xl"></i>
+                <i className="devicon-typescript-plain colored md:text-7xl text-4xl"></i>
+                <i className="devicon-html5-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-css3-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-react-original colored md:text-7xl text-4xl"></i>
+                <i className="devicon-tailwindcss-original colored md:text-7xl text-4xl"></i>
+                <i className="devicon-materialui-plain colored md:text-7xl text-4xl"></i>
+                <i className="devicon-jest-plain colored md:text-7xl text-4xl"></i>
+                <i className="devicon-nodejs-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-prisma-original md:text-7xl text-4xl"></i>
+                <i className="devicon-adonisjs-original md:text-7xl text-4xl colored"></i>
+                <i className="devicon-nestjs-original colored md:text-7xl text-4xl"></i>
+                <i className="devicon-express-original md:text-7xl text-4xl"></i>
+                <i className="devicon-postgresql-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-mysql-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-mongodb-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-microsoftsqlserver-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-docker-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-linux-plain md:text-7xl text-4xl" ></i>
+                <i className="devicon-git-plain-wordmark colored md:text-7xl text-4xl"></i>
+                <i className="devicon-gitlab-plain-wordmark colored md:text-7xl text-4xl"></i>
             </div>
         </div>
     )
