43b909ec84dc36e71a8daee805c5d8e3dc18079c
BernardoMoschen <bernardomoschen.dev@gmail.com>
2025-05-09 11:51:11 -0300

    style: intro and who

--- src/components/react/Button.tsx
+++ src/components/react/Button.tsx
@@ -9,8 +9,8 @@ type ButtonProps = {
 export const Button = ({ label, href, className, ...restProps }: ButtonProps) => {
     return (
         <a href={href}
-            className={cn(className, "cursor-pointer rounded px-5 py-2.5 overflow-hidden group bg-tangelo-500 relative hover:bg-gradient-to-r hover:from-tangelo-600 hover:to-tangelo-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-tangelo-400 transition-all ease-out duration-300")}>
-            <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-amber-500 opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+            className={cn(className, "cursor-pointer rounded px-5 py-2.5 overflow-hidden  bg-tangelo-500 relative hover:bg-gradient-to-r hover:from-tangelo-600 hover:to-tangelo-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-tangelo-400 transition-all ease-out duration-300")}>
+            <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-amber-500 opacity-10 rotate-12 -hover:-translate-x-40 ease"></span>
             <span className="relative">{label}</span>
         </a>
     );
--- src/components/react/ClipboardButton.tsx
+++ src/components/react/ClipboardButton.tsx
@@ -22,9 +22,9 @@ export const ClipboardButton = ({ label, ...restProps }: ButtonProps) => {
         <button
             onClick={handleCopyClick}
             {...restProps}
-            className="rounded px-5 py-2.5 overflow-hidden group bg-tangelo-500 relative hover:bg-gradient-to-r hover:from-tangelo-600 hover:to-tangelo-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-tangelo-400 transition-all ease-out duration-300 cursor-pointer"
+            className="rounded px-5 py-2.5 overflow-hidden  bg-tangelo-500 relative hover:bg-gradient-to-r hover:from-tangelo-600 hover:to-tangelo-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-tangelo-400 transition-all ease-out duration-300 cursor-pointer"
         >
-            <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-amber-500 opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+            <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-amber-500 opacity-10 rotate-12 -hover:-translate-x-40 ease"></span>
             <span className="relative">{isCopied ? 'Copied!' : label}</span>
         </button>
     );
